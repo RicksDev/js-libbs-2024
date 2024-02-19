3c43d617946a828dc3e370101641a2878715424d
Luiz Ricardo Filho <filho.ricardo13@gmail.com>
2024-02-19 15:58:53 -0300

    Terminando o projeto

--- aula-04/script.js
+++ aula-04/script.js
@@ -1,16 +1,56 @@
 //pegar os dados do formulario
 
 const fname = document.getElementById("fname");
+const ename = document.getElementById("ename");
+const tname = document.getElementById("tname");
 
-//Para ouvir o click do botão, usamos o addEventListener
+//Para "ouvir" o click do botão, usamos o addEventListener
 
 const btnSubmit = document.getElementById("btn");
+
+//Criar uma lista vazia
+
+const data = [];
 
 btnSubmit.addEventListener('click', function (event) {
     //Não deixa o formulário se comportar como padrão, ou seja, 
     //enviar os dados para próxima página/requisição
     event.preventDefault();
 
-    console.log(fname.value);
-    console.log(lname.value);
+    const pessoa = {
+        nome: fname.value,
+        endereco: ename.value,
+        fone: tname.value,
+
+        favoritos: [
+            {
+                titulo: "A nova casa",
+                autor:"Juca bala",
+                ano:2025,
+                genero:"Aventura"
+            }
+        ]
+    };
+
+    data.push(pessoa);
+
+    console.log(data);
+
+    console.log(pessoa);
+
+    console.log("======================================");
+
+    console.log(pessoa.favoritos);
+
+    // console.log(fname.value);
+    // console.log(ename.value);
+    // console.log(tname.value);
+
+    const dados = [
+        fname.value,
+        ename.value,
+        tname.value
+    ];
+
+    console.log(dados);
 }); 
